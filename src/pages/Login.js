df993133cdc9984164ac595fd19c41030793b7d7
jjh0491 <jjh@foodnet24.com>
2023-03-13 17:36:19 +0900

    로그인화면 작업중

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,17 @@ const Login = () => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="logo1" />
+          <SignUp>get all there</SignUp>
+          <Description>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus
+            exercitationem ullam quas ab cumque saepe doloremque nam est
+            laboriosam. Similique enim iste velit minus sint laborum rerum
+            voluptatem, eaque dolorum.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="logo2" />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -45,5 +56,50 @@ const BgImage = styled.div`
   background-repeat: no-repeat;
   z-index: -1;
 `;
+
+const CTA = styled.div`
+  width: 100%;
+  max-width: 650px;
+  display: flex;
+  flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  z-index: 10;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #f9f9f9;
+  background: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0,0,95.3%,1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`
+
+const CTALogoTwo = styled.img`
+
+`
 
 export default Login;
